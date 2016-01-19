c459d76e8ffa87aaae83cea989bf8a89c01be6b7
emottk <emily.kowal@postlight.com>
2016-01-19 14:52:24 -0500

    change Artist and Opinion models

--- newmusic/main/models.py
+++ newmusic/main/models.py
@@ -4,14 +4,19 @@ from django.conf import settings
 
 
 class Artist(models.Model):
-    name = models.CharField
+    name = models.CharField(max_length=100)
+    sc_id = models.IntegerField()
 
+    # @classmethod
+    # def create(cls, name):
+    #     artist = cls(name=name)
 
 class Song(models.Model):
-    name = models.CharField
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    artist = models.ForeignKey('main.Artist', on_delete=models.CASCADE)
+
 
 class Opinion(models.Model):
-    opinion = models.BooleanField
+    opinion = models.BooleanField()
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    # artist = models.ForeignKey('main.Artist', on_delete=models.CASCADE)
