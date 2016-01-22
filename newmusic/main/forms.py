426d870d03fc19c4d91645df6e74cd18b4849857
emottk <emily.kowal@postlight.com>
2016-01-22 16:54:09 -0500

    form for opinion saving

--- newmusic/main/forms.py
+++ newmusic/main/forms.py
@@ -1,12 +1,16 @@
 from django.forms import ModelForm
+from django import forms
 
 from newmusic.main.models import Opinion
 
 class OpinionForm(ModelForm):
+    opinion = forms.BooleanField(required=False)
+
     class Meta:
         model = Opinion
-        fields = ['opinion']
+        fields = ['opinion', 'artist']
 
-    def form_valid(self, form):
-        form.user = self.request.user
-        return super(OpinionForm, self).form_valid(form)
+    # def form_valid(self, form):
+    #     form.user = self.request.user
+    #     form.artist = self.request.artist
+    #     return super(OpinionForm, self).form_valid(form)
