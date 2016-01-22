a15eb34db60a941c7801edcbe13632c53cc0df3b
emottk <emily.kowal@postlight.com>
2016-01-22 16:54:48 -0500

    update urls

--- newmusic/users/urls.py
+++ newmusic/users/urls.py
@@ -8,8 +8,8 @@ from . import views
 urlpatterns = patterns('',
     # the view to register our user with a third party token
     # the backend is the python social auth backend e.g. facebook
-    url(r'^register-by-token/(?P<backend>[^/]+)/$', views.register_by_access_token),
-    url(r'^check/', views.check),
-    url(r'^home/', views.home),
-    url(r'^callback/', views.callback),
+    url(r'^register-by-token/(?P<backend>[^/]+)/$', views.UserView.register_by_access_token),
+    url(r'^check/', views.UserView.get),
+    url(r'^home/', views.UserView.home),
+    url(r'^callback/', views.UserView.callback),
 )
