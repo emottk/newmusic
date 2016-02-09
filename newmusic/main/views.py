31cb6120ed80d075daf1ca50f2274d5574c90ae0
emottk <emily.kowal@postlight.com>
2016-02-09 17:05:31 -0500

    Fix #23

--- newmusic/main/views.py
+++ newmusic/main/views.py
@@ -1,6 +1,6 @@
 
 from django.contrib.auth.decorators import login_required
-from django.http import Http404, HttpResponse
+from django.http import HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.generic import View
