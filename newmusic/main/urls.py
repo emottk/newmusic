36df0c3bbcd1df9328a1a966099a16cd98a32bda
emottk <emily.kowal@postlight.com>
2016-02-24 18:14:59 -0500

    tweak template ++ home url change

--- newmusic/main/urls.py
+++ newmusic/main/urls.py
@@ -4,7 +4,7 @@ from newmusic.main import views
 
 
 urlpatterns = patterns('',
-    url(r'^home/$', views.ArtistIndex.as_view(), name="explore"),
+    url(r'^$', views.ArtistIndex.as_view(), name="explore"),
     url(r'^about/$', views.AboutIndex.as_view(), name="about"),
     url(r'^artist/(?P<artist>[\w.@+-]+)/$', views.ArtistPage.as_view(), name="artist_page"),
     url(r'^opinion/delete/(?P<pk>\d+)$', views.OpinionDelete.as_view(), name="opinion_delete"),
